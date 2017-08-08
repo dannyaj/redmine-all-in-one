db4cab79a576e685948e19019459fafdca63fb5f
Danny AJ <linshengte@gmail.com>
2017-08-08 17:35:14 +0800

    Add files via upload

--- mnt/redmine/docker/start.sh
+++ mnt/redmine/docker/start.sh
@@ -4,6 +4,7 @@ chown -R mysql:mysql /var/lib/mysql
 
 /etc/init.d/mysql start
 /etc/init.d/apache2 start
+/etc/init.d/rsyslog start
 
 mysql -u root < /opt/init_redmine.sql
 mysqladmin -u root password password
@@ -12,3 +13,4 @@ cd redmine/
 rake generate_secret_token
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake redmine:load_default_data
+passenger start
